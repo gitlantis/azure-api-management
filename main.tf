3b88d6ba0ea0a800e49afc5a896764ae1f22b99b
gitlantis <39701194+gitlantis@users.noreply.github.com>
2024-10-08 23:01:03 +0500

    get post enabled

--- main.tf
+++ main.tf
@@ -13,21 +13,14 @@ resource "azurerm_api_management" "api_management" {
 }
 
 resource "azurerm_api_management_api" "api" {
-  name                = "api_management_api_${var.resource_group.name}"
-  resource_group_name = azurerm_resource_group.rg.name
-  api_management_name = azurerm_api_management.api_management.name
-  revision            = "1"
-  display_name        = "API to External Service"
-  path                = ""
-  protocols           = ["https"]
-}
-
-resource "azurerm_api_management_backend" "external_backend" {
-  name                = "external_service_${var.resource_group.name}"
-  resource_group_name = azurerm_resource_group.rg.name
-  api_management_name = azurerm_api_management.api_management.name
-  url                 = var.base_url
-  protocol            = "http"
+  name                  = "api_management_api_${var.resource_group.name}"
+  resource_group_name   = azurerm_resource_group.rg.name
+  api_management_name   = azurerm_api_management.api_management.name
+  revision              = "1"
+  display_name          = "API to External Service"
+  path                  = ""
+  protocols             = ["https"]
+  subscription_required = false
 }
 
 resource "azurerm_api_management_api_operation" "get_operation" {
@@ -41,7 +34,7 @@ resource "azurerm_api_management_api_operation" "get_operation" {
 
   template_parameter {
     name        = "id"
-    required    = false
+    required    = true
     type        = "string"
     description = "user GUId"
   }
@@ -65,7 +58,6 @@ resource "azurerm_api_management_api_operation" "post_operation" {
     description = "login trquest"
     representation {
       content_type = "application/json"
-
     }
   }
 
@@ -84,21 +76,38 @@ resource "azurerm_api_management_api_operation_policy" "get_policy" {
   xml_content = <<XML
 <policies>
     <inbound>
+        <set-variable name="guid" value="@(context.Request.MatchedParameters["id"])" />
+
+        <return-response>
+            <set-status code="200" reason="OK" />
+            <set-header name="Content-Type" exists-action="override">
+                <value>application/json</value>
+            </set-header>
+            <set-body>@{
+                return new JObject(
+                    new JProperty("message", "This is a simple echo response"),
+                    new JProperty("status", "success"),
+                    new JProperty("id", context.Variables["guid"])
+                ).ToString();
+            }</set-body>
+        </return-response>
         <base />
-        <validate-jwt header-name="Authorization" failed-validation-httpcode="400" failed-validation-error-message="Bad Request">            
-        </validate-jwt>
-        <set-backend-service base-url="${var.base_url}/User/GetUser?guid={id}" />
+
     </inbound>
+
     <backend>
         <base />
     </backend>
+
     <outbound>
         <base />
     </outbound>
+
     <on-error>
         <base />
     </on-error>
 </policies>
+
 XML
 }
 
@@ -111,18 +120,39 @@ resource "azurerm_api_management_api_operation_policy" "post_policy" {
   xml_content = <<XML
 <policies>
     <inbound>
+        <set-variable name="body" value="@(context.Request.Body.As<string>(preserveContent: true))" />
+        
+        <return-response>
+            <set-status code="200" reason="OK" />
+            <set-header name="Content-Type" exists-action="override">
+                <value>application/json</value>
+            </set-header>
+            <set-body>@{
+                return new JObject(
+                    new JProperty("message", "This is a simple echo response"),
+                    new JProperty("status", "success"),
+                    new JProperty("body", context.Variables["body"])
+                ).ToString();
+            }</set-body>
+        </return-response>
         <base />
-        <set-backend-service base-url="${var.base_url}/User/Login" />
     </inbound>
+
     <backend>
         <base />
     </backend>
+
     <outbound>
         <base />
+        <set-header name="Content-Type" exists-action="override">
+            <value>application/json</value>
+        </set-header>
     </outbound>
+
     <on-error>
         <base />
     </on-error>
 </policies>
 XML
 }
+
--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
 output "azurerm_api_management_api" {
   value = azurerm_api_management_api.api
 }
-
-output "azurerm_api_management_backend" {
-  value = azurerm_api_management_backend.external_backend
-}
